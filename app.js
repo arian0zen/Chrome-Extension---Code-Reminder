c9bb01410dc67afc5cf465f67ac1bf5d4cec1e57
Arian Shaikh <ariansk90@gmail.com>
2022-08-29 21:59:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -42,3 +42,97 @@ fetch(CodingNinjas_API_ENDPOINT)
 
 
     });
+
+//syncLeetCodeCodingChallenge
+
+    const LEETCODE_API_ENDPOINT = "https://leetcode.com/graphql";
+const DAILY_CODING_CHALLENGE_QUERY = `
+query questionOfToday {
+	activeDailyCodingChallengeQuestion {
+		date
+		userStatus
+		link
+		question {
+			acRate
+			difficulty
+			freqBar
+			frontendQuestionId: questionFrontendId
+			isFavor
+			paidOnly: isPaidOnly
+			status
+			title
+			titleSlug
+			hasVideoSolution
+			hasSolution
+			topicTags {
+				name
+				id
+				slug
+			}
+		}
+	}
+}`;
+
+
+/*Sync LeetCode daily coding challenge to Todoist*/
+
+// getLeetCode = async()=>{
+//     const init = {
+//         method: "POST",
+//         headers: { "Content-Type": "application/json", "Host": "leetcode.com", "Origin": "https://leetcode.com", "Sec-Fetch-Mode": "cors", "Sec-Fetch-Dest": "empty"  },
+//         body: JSON.stringify({ query: DAILY_CODING_CHALLENGE_QUERY })
+        
+        
+//       };
+
+//       try{
+//         const fetchResponse = await fetch(LEETCODE_API_ENDPOINT, init);
+//         const data = await fetchResponse.json();
+//         console.log(data)
+//         return data;
+//       }
+//       catch(e){
+//         return e;
+//       }
+// }
+
+// getLeetCode();
+
+
+
+// const syncLeetCodeCodingChallenge = async () => {
+//   const question = await fetchDailyCodingChallenge();
+//   console.log("LeetCode: POTD");
+
+//   const questionInfo = question.data.activeDailyCodingChallengeQuestion;
+
+
+//   const questionTitle = questionInfo.question.title;
+//   const questionDifficulty = questionInfo.question.difficulty;
+//   const questionLink = `https://leetcode.com${questionInfo.link}`;
+//   console.log("the problem of the day for", questionInfo.date);
+//   console.log("problem name:", questionInfo.question.title);
+//   console.log("problem description:", questionInfo.question.difficulty);
+//   console.log("problem link: leetcode.com" + questionInfo.link);
+//   console.log("***********")
+//   console.log("***********")
+//   console.log("   ");
+
+
+// };
+
+// const fetchDailyCodingChallenge = async () => {
+//   console.log(`Fetching daily coding challenge from LeetCode API.`);
+
+//   const init = {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({ query: DAILY_CODING_CHALLENGE_QUERY }),
+//   };
+
+//   const response = await fetch(LEETCODE_API_ENDPOINT, init);
+
+//   return response.json();
+// };
+
+// syncLeetCodeCodingChallenge();
